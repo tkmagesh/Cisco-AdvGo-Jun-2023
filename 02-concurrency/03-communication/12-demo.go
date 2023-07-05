053f5b68e2f1a28e6cb52b1b1cc920a42fc5070f
Magesh Kuppan <tkmagesh77@gmail.com>
2023-07-05 17:01:54 +0530

    Day-01 complete

--- 02-concurrency/03-communication/12-demo.go
+++ 02-concurrency/03-communication/12-demo.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"time"
 )
 
 // share memory by communicating
@@ -10,22 +10,16 @@ import (
 var result int
 
 func main() {
-	/*
-		var ch chan int
-		ch = make(chan int)
-	*/
+
 	ch := make(chan int)
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	go add(100, 200, wg, ch)
+	go add(100, 200, ch)
 	result := <-ch
-	wg.Wait()
 	fmt.Println("result :", result)
 
 }
 
-func add(x, y int, wg *sync.WaitGroup, ch chan int) {
+func add(x, y int, ch chan int) {
 	result := x + y
+	time.Sleep(5 * time.Second)
 	ch <- result
-	wg.Done()
 }
---
+++ 02-concurrency/03-communication/13-demo.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// share memory by communicating
+var result int
+
+// consumer
+func main() {
+	ch := add(100, 200)
+	/*
+		go func() {
+			ch <- 10000
+		}()
+	*/
+	result := <-ch
+	fmt.Println("result :", result)
+}
+
+// producer
+func add(x, y int) <-chan int {
+	ch := make(chan int)
+	go func() {
+		result := x + y
+		time.Sleep(2 * time.Second)
+		ch <- result
+	}()
+	return ch
+}
